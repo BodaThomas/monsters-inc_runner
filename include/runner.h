57a0a2f0d2c4a53e17b0298e4e6855f39d2e238d
Thomas Boda <thomas.boda@epitech.eu>
2019-01-15 19:08:21 +0100

    Add the .h file of the project

---
+++ include/runner.h
@@ -0,0 +1,10 @@
+/*
+** EPITECH PROJECT, 2019
+** MUL_my_runner_2018
+** File description:
+** The .h file of the my_runner project
+*/
+
+int game(void);
+void help(void);
+void print_error(int mode);
